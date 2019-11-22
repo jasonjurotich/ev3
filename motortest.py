5cb4103c136eb14e5bc91a44aeb502cc4f12b889
Jason Jurotich <jj@jjir.org>
2019-11-22 11:44:35 -0600

    Update motortest.py

--- motortest.py
+++ motortest.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, SpeedPercent
 
 m = LargeMotor(OUTPUT_A)
 m.on_for_rotations(SpeedPercent(75), 5)
